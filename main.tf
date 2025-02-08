9e7d68024599fba069d2071701e743fc8ad17c44
sureshullagaddi <suresh.blr61@gmail.com>
2025-02-08 21:46:12 +0100

    creating ec2 instance before we need to create aws vpc and subnet

---
+++ main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami                     = "ami-085ad6ae776d8f09c"
+  instance_type           = "t2.micro"
+  subnet_id = "subnet-044f9dd8495d46059"
+}
