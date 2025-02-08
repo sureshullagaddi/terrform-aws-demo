d8aa856e1fac5977ad0411367d6050a6567bc3b7
sureshullagaddi <suresh.blr61@gmail.com>
2025-02-08 21:47:42 +0100

    added comment

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami                     = "ami-085ad6ae776d8f09c"
+  ami                     = "ami-085ad6ae776d8f09c" # check in aws console the ami name
   instance_type           = "t2.micro"
-  subnet_id = "subnet-044f9dd8495d46059"
+  subnet_id = "subnet-044f9dd8495d46059" #create aws vpc and give the subnet id
 }
